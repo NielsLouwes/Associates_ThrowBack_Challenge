c735eb45459201be60aae816b4bb9db108aed7a1
Niels Louwes <76694402+NielsLouwes@users.noreply.github.com>
2023-01-13 12:41:27 +0100

    Merged arrays

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import { squadsData } from "./squads";
 
 export default function App() {
   function findTopThreeOverall(data) {
+    let players = [];
     for (const [key, value] of Object.entries(data)) {
       const squads = value.map((item) => {
-        return { name: item.name, score: item.score.reduce((a, b) => a + b) };
+        return {
+          squad: key,
+          name: item.name,
+          score: item.score.reduce((a, b) => a + b)
+        };
       });
 
       console.log("squads", squads);
 
       const sortedScores = squads.sort((a, b) => b.score - a.score); // Sorted within each squad so far
-      const firstInSquad = sortedScores[0];
-      console.log("sortedScores", sortedScores);
-      console.log("first in SQuad", firstInSquad);
 
-      //trying ot merge them
+      players.push(squads); // returnsan array of arrays with objects
+      // trying to flatten this now to get all in one thing
+      console.log(players);
+      const newArray = players.flat(); // new flattened array now has one array with many objects
+      console.log("new Array:", newArray);
     }
   }
 
@@ -22,3 +28,8 @@ export default function App() {
 
   return <div className="App"></div>;
 }
+
+// const arr1 = [0, 1, 2, [3, 4]];
+
+// console.log(arr1.flat());
+// // Expected output: Array [0, 1, 2, 3, 4]
