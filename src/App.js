f7edc343d7ee3cd457f29cae8710e4a67f9b9be8
Niels Louwes <76694402+NielsLouwes@users.noreply.github.com>
2023-01-27 16:37:01 +0100

    Worked on most picked activities

--- src/App.js
+++ src/App.js
@@ -7,35 +7,34 @@ export default function App() {
 
   function FindTopThreeChallenges(data) {
     for (const [key, value] of Object.entries(data)) {
-      let players = [];
-      const squads = value.map((item) => {
-        const maxPlayerScore = Math.max(...item.score); // returns max score
-        console.log("max player score:", maxPlayerScore);
+      let playerMostPickedActivity = [];
+      const squads = value.map((squadMember) => {
+        const maxPlayerScore = Math.max(...squadMember.score); // returns max score
+        // console.log("max player score:", maxPlayerScore);
         if (maxPlayerScore !== 0) {
           // filtering out those who didn't participate
-          const maxValueIndex = item.score.indexOf(maxPlayerScore); //returns index of where Max score is being found in each person's list of scores
-          return {
-            squad: key,
-            name: item.name,
-            topChallengeIndex: maxValueIndex
-          };
+          const maxValueIndex = squadMember.score.indexOf(maxPlayerScore); //returns index of where Max score is being found in each person's list of scores
+
+          playerMostPickedActivity.push(maxValueIndex);
+          console.log("maxValueIndex", maxValueIndex);
         }
       });
-      // console.log("maxPlayerScore:", maxPlayerScore);
-      players.push(squads); // we now have objects for each players with their most played challenge
-      const newArray = players.flat(); //this returns the indexes of most occuring highest scores
-
-      // console.log("Index of MAX Score", newArray);
+      console.log("playersMostPickedActivity:", playerMostPickedActivity);
+      const result = {
+        name: key,
+        total: playerMostPickedActivity
+      };
+      return result;
     }
   }
 
-  const FindTopScoringSquads = (data) => {
+  const FindTopScoringSquads2 = (data) => {
     let scores = [];
     const firstSquad = data.Cohen.map((members) => {
-      // console.log("members", members); // return all members of Cohen
+      console.log("members", members); // return all members of Cohen
 
       scores = members.score.reduce((a, b) => a + b);
-      // console.log("scores", scores);
+      console.log("scores", scores);
       return scores;
     });
 
@@ -46,7 +45,21 @@ export default function App() {
     return result;
   };
 
-  FindTopScoringSquads(squadsData);
+  const FindTopScoringSquads = (data) => {
+    for (const squad in squadsData) {
+      // console.log("squad:", squad);
+      for (const person of squadsData[squad]) {
+        // console.log("squadsData", squadsData[squad]);
+        let totalScore = 0;
+        for (const score of person.score) {
+          totalScore += score;
+        }
+        console.log(`${person.name}'s total score is ${totalScore}`);
+      }
+    }
+  };
+
+  FindTopThreeChallenges(squadsData);
 
   return <></>;
 }
