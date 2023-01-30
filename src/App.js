ea7b91965cbd60f88e1af4f86c4215d8b63832fe
Niels Louwes <76694402+NielsLouwes@users.noreply.github.com>
2023-01-30 13:36:55 +0100

    Worked on top three challenge and added to do

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ export default function App() {
 
   function FindTopThreeChallenges(data) {
     for (const [key, value] of Object.entries(data)) {
+      console.log("VALUE", value); // value is only returning the first object so only first
+      let scores = [];
       let playerMostPickedActivity = [];
       const squads = value.map((squadMember) => {
         const maxPlayerScore = Math.max(...squadMember.score); // returns max score
         // console.log("max player score:", maxPlayerScore);
-        if (maxPlayerScore !== 0) {
-          // filtering out those who didn't participate
-          const maxValueIndex = squadMember.score.indexOf(maxPlayerScore); //returns index of where Max score is being found in each person's list of scores
-
-          playerMostPickedActivity.push(maxValueIndex);
-          console.log("maxValueIndex", maxValueIndex);
-        }
+        // filtering out those who didn't participate
+        // console.log("squad member:", squadMember);
+        const maxValueIndex = squadMember.score.indexOf(maxPlayerScore); //returns index of where Max score is being found in each person's list of scores
+        scores.push(maxPlayerScore); //its only pushing high scores for 1st squad
+        playerMostPickedActivity.push(maxValueIndex); // only pushing scores for 1st squad
+        // console.log("maxValueIndex", maxValueIndex);
+        // console.log("scores:", scores);
+        // if (maxPlayerScore !== 0) {
+        // }
       });
-      console.log("playersMostPickedActivity:", playerMostPickedActivity);
+      // console.log("playersMostPickedActivity:", playerMostPickedActivity);
       const result = {
         name: key,
         total: playerMostPickedActivity
@@ -27,6 +31,27 @@ export default function App() {
       return result;
     }
   }
+
+  const findTopThreeChallenges2 = (data) => {
+    Object.entries(data).forEach((eachSquad, key) => {
+      // console.log(eachSquad)
+      // 1 (top level) logics
+      let total = 0;
+      eachSquad.map((eachSquadMember) => {
+        // top level + inner level
+        // 1, 1.1, 1.2, 1.3
+        console.log(eachSquadMember);
+        // total = eachSquadMember.score.reduce(())
+      });
+
+      // 1 = aletta = { name, total}
+      // 2 = koolhaas = { name, total }
+      const result = {
+        name: eachSquad[key],
+        total
+      };
+    });
+  };
 
   const FindTopScoringSquads2 = (data) => {
     let scores = [];
@@ -46,20 +71,34 @@ export default function App() {
   };
 
   const FindTopScoringSquads = (data) => {
-    for (const squad in squadsData) {
-      // console.log("squad:", squad);
-      for (const person of squadsData[squad]) {
-        // console.log("squadsData", squadsData[squad]);
-        let totalScore = 0;
-        for (const score of person.score) {
-          totalScore += score;
-        }
-        console.log(`${person.name}'s total score is ${totalScore}`);
-      }
-    }
+    // all squad
+    Object.entries(data).forEach((eachSquad, key) => {
+      console.log("EACH SQUAD:", eachSquad);
+      // 1 (top level) logics
+      let total = 0;
+      eachSquad.map((eachSquadMember, key) => {
+        // top level + inner level
+        // 1, 1.1, 1.2, 1.3
+        console.log("EACH SQUAD MEMBER:", eachSquadMember);
+      });
+
+      // 1 = aletta = { name, total}
+      // 2 = koolhaas = { name, total }
+      const result = {
+        name: eachSquad[key],
+        total
+      };
+    });
   };
 
   FindTopThreeChallenges(squadsData);
 
   return <></>;
 }
+
+/* TO DO (FindTopThreeChallenges)
+1. figure out why its only iterating over Cohen
+2. need to map over all values, not just Cohen
+
+
+*/
